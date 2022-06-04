0b18c5b3201f69dd0904d89212ac852456e933ca
lucian1333 <mlmlucian@gmail.com>
2022-06-03 20:07:35 -0500

    resolved code status 1

---
+++ src/main/java/Mentoring/com/test/googleSearch/pages/LandingPage.java
@@ -0,0 +1,37 @@
+package Mentoring.com.test.googleSearch.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LandingPage {
+
+    public LandingPage(WebDriver driver) {
+
+        PageFactory.initElements(driver , this);
+
+    }
+
+    @FindBy (xpath = "//input[@name='q']")
+    WebElement searchFiled;
+
+    @FindBy (xpath = "//input[@name='btnK']")
+    WebElement searchButton;
+
+    @FindBy (xpath = "//a[@aria-label='Sign in']//../button")
+    WebElement signInPopUpNoThanksButton;
+
+    public void signInNoThanks () {
+        this.signInPopUpNoThanksButton.click();
+    }
+
+    public void searchOnGoogle(String text) throws InterruptedException {
+        this.searchFiled.sendKeys(text);
+        Thread.sleep(2500);
+        this.searchButton.click();
+    }
+
+
+
+}
---
+++ src/main/java/Mentoring/com/test/googleSearch/tests/LandingTests.java
@@ -0,0 +1,39 @@
+package Mentoring.com.test.googleSearch.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import Mentoring.com.test.googleSearch.pages.LandingPage;
+
+public class LandingTests {
+
+    WebDriver driver = null;
+
+    @BeforeMethod
+
+    public void setup () {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+    }
+
+    @Test
+
+    public void validateHappyPath () throws InterruptedException {
+
+        LandingPage landingPage = new LandingPage(driver);
+        Thread.sleep(3000);
+        landingPage.signInNoThanks();
+        Thread.sleep(3000);
+        landingPage.searchOnGoogle("automation step by step");
+        Thread.sleep(2500);
+        Assert.assertTrue(driver.getCurrentUrl().equals("automation step by step - Google Search"));
+    }
+
+
+}
